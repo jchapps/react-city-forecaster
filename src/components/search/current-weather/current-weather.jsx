0c7e4707178553c4d1e23362657ac9828b1f83d7
Josh Chappelow <jdchappelow@gmail.com>
2022-10-17 17:01:03 +0900

    feat: weather card styling

--- src/components/search/current-weather/current-weather.jsx
+++ src/components/search/current-weather/current-weather.jsx
@@ -4,10 +4,36 @@ const CurrentWeather = () => {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city">London</p>
-        <p className="weather">Cloudy</p>
+        <div>
+          <p className="city">London</p>
+          <p className="weather-type">Cloudy</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
       </div>
-      <img alt="weather" className="weather-icon" src="icons/01d.png" />
+      <div className="bottom">
+        <p className="temperature">5C</p>
+        <div className="details">
+          <div className="para-row">
+            <span className="para-label top">Details</span>
+          </div>
+          <div className="para-row">
+            <span className="para-label">Feels like</span>
+            <span className="para-value">22C</span>
+          </div>
+          <div className="para-row">
+            <span className="para-label">Wind</span>
+            <span className="para-value">2m/s</span>
+          </div>
+          <div className="para-row">
+            <span className="para-label">Humidity</span>
+            <span className="para-value">15%</span>
+          </div>
+          <div className="para-row">
+            <span className="para-label">Pressure</span>
+            <span className="para-value">15hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
