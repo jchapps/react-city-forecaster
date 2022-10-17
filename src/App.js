d3a7888d3965be2d9d763820c61166885b67e7ec
Josh Chappelow <jdchappelow@gmail.com>
2022-10-17 16:02:58 +0900

    feat: geo api returning coords

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Search from './components/search/Search';
+
+import "./App.css";
+import Search from "./components/search/Search";
 
 function App() {
-
-  const handleOnSearchChange= (searchData) => {
-    console.log(searchData)
-  }
-
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  };
 
   return (
     <div className="container">
-      <Search onSearchChange={handleOnSearchChange}/>
+      <Search onSearchChange={handleOnSearchChange} />
     </div>
   );
 }
---
+++ src/api.jsx
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4d0a7c9371msh26f61afa09a8270p1dbc11jsn216eb856392c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+}
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
