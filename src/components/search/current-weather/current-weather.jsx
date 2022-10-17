e8634469ccb4f249ac2207accc52260958f155ac
Josh Chappelow <jdchappelow@gmail.com>
2022-10-17 21:43:23 +0900

    feat: dynamic weather card

--- src/components/search/current-weather/current-weather.jsx
+++ src/components/search/current-weather/current-weather.jsx
@@ -11,26 +11,26 @@ const CurrentWeather = ({data}) => {
         <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
       </div>
       <div className="bottom">
-        <p className="temperature">5C</p>
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
         <div className="details">
           <div className="para-row">
             <span className="para-label top">Details</span>
           </div>
           <div className="para-row">
             <span className="para-label">Feels like</span>
-            <span className="para-value">22C</span>
+            <span className="para-value">{Math.round(data.main.feels_like)}°C</span>
           </div>
           <div className="para-row">
             <span className="para-label">Wind</span>
-            <span className="para-value">2m/s</span>
+            <span className="para-value">{data.wind.speed} m/s</span>
           </div>
           <div className="para-row">
             <span className="para-label">Humidity</span>
-            <span className="para-value">15%</span>
+            <span className="para-value">{data.main.humidity}%</span>
           </div>
           <div className="para-row">
             <span className="para-label">Pressure</span>
-            <span className="para-value">15hPa</span>
+            <span className="para-value">{data.main.pressure} hPa</span>
           </div>
         </div>
       </div>
